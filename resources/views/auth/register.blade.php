9b9e270bf5b3e555fa4e4d63516474acb6c3928b
Cedric Puchalver <cedric.puchalver@gmail.com>
2024-12-09 18:27:10 +0100

    Changement de design des pages login et register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,51 +2,99 @@
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
-        </div>
+        <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
+            <div class="p-4 sm:p-7">
+                <div class="text-center">
+                    <h1 class="block text-2xl font-bold text-gray-800">Sign up</h1>
+                    <p class="mt-2 text-sm text-gray-600">
+                        Already have an account?
+                        <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="{{ route('login') }}">
+                        Sign in here
+                        </a>
+                    </p>
+                </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+                <div class="mt-5">
+                    <!-- Form -->
+                    <form>
+                        <div class="grid gap-y-4">
+                            <!-- Form Group -->
+                            <div>
+                                <label for="name" class="block text-sm mb-2">Name</label>
+                                <div class="relative">
+                                    <input type="text" id="name" name="name" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="name-error">
+                                    <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                        <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <p class="hidden text-xs text-red-600 mt-2" id="name-error">Please include your name</p>
+                            </div>
+                            <!-- End Form Group -->
+                             
+                            <!-- Form Group -->
+                            <div>
+                                <label for="email" class="block text-sm mb-2">Email address</label>
+                                <div class="relative">
+                                    <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="email-error">
+                                    <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                        <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                            </div>
+                            <!-- End Form Group -->
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+                            <!-- Form Group -->
+                            <div>
+                                <label for="password" class="block text-sm mb-2">Password</label>
+                                <div class="relative">
+                                    <input type="password" id="password" name="password" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="password-error">
+                                    <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                        <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
+                            </div>
+                            <!-- End Form Group -->
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
+                            <!-- Form Group -->
+                            <div>
+                                <label for="password_confirmation" class="block text-sm mb-2">Confirm Password</label>
+                                <div class="relative">
+                                    <input type="password" id="confirm-password" name="password_confirmation" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="confirm-password-error">
+                                    <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                        <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                        </svg>
+                                    </div>
+                                </div>
+                                <p class="hidden text-xs text-red-600 mt-2" id="confirm-password-error">Password does not match the password</p>
+                            </div>
+                            <!-- End Form Group -->
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+                            <!-- Checkbox -->
+                            <div class="flex items-center">
+                                <div class="flex">
+                                    <input id="remember-me" name="remember-me" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500">
+                                </div>
+                                <div class="ms-3">
+                                    <label for="remember-me" class="text-sm">I accept the <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="#">Terms and Conditions</a></label>
+                                </div>
+                            </div>
+                            <!-- End Checkbox -->
 
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
+                            <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">Sign up</button>
+                        </div>
+                    </form>
+                    <!-- End Form -->
+                </div>
+            </div>
         </div>
     </form>
 </x-guest-layout>
