041385813a4c1bab333bfab79d37696278361115
Cedric Puchalver <cedric@season-of-mist.com>
2025-02-17 05:30:06 +0100

    Fichiers de traduction anglaise

---
+++ lang/en/auth.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'These credentials do not match our records.',
+    'password' => 'The provided password is incorrect.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    'login' => [
+        'heading' => 'Sign in',
+        'no_account' => 'Don\'t have an account yet?',
+        'signup' => 'Sign up here',
+        'email' => 'Email address',
+        'password' => 'Password',
+        'forgot_password' => 'Forgot your password?',
+        'remember' => 'Remember me',
+        'button' => 'Sign in',
+    ],
+
+    'register' => [
+        'heading' => 'Sign up',
+        'already_account' => 'Already have an account?',
+        'signin' => 'Sign in here',
+        'name' => 'Name',
+        'email' => 'Email address',
+        'password' => 'Password',
+        'password_confirmation' => 'Confirm Password',
+        'button' => 'Sign up',
+    ],
+
+    'forgot-password' => [
+        'heading' => 'Forgot password?',
+        'remember_password' => 'Remember your password?',
+        'signin' => 'Sign in here',
+        'email' => 'Email address',
+        'button' => 'Reset password',
+    ],
+
+    'reset-password' => [
+        'email' => 'Email address',
+        'password' => 'Password',
+        'password_confirmation' => 'Confirm Password',
+        'button' => 'Reset Password',
+    ],
+
+    'confirm-password' => [
+        'confirm_before' => 'This is a secure area of the application. Please confirm your password before continuing.',
+        'password' => 'Password',
+    ],
+
+    'verify-email' => [
+        'heading' => 'Verify email',
+        'message' => 'A new verification link has been sent to the email address you provided during registration.',
+        'thanks' => 'Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you?',
+        'resend' => 'If you didn\'t receive the email, we will gladly send you another.',
+        'button' => 'Resend Verification Email',
+    ],
+];
