9134d819be4da337b9580f5dc8ffa6f63fcd13d8
Cedric Puchalver <cedric@season-of-mist.com>
2025-02-17 05:24:10 +0100

    Création RegisterRequest

---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+            'password_confirmation' => ['required'],
+            'accept_terms' => ['accepted'],
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array<string, string>
+     */
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Please enter your name',
+            'email.required' => 'Please enter a valid email address',
+            'password.required' => 'Please enter a password',
+            'password.min:8' => 'The password must be at least 8 characters',
+            'password_confirmation.required' => 'Please confirm the password',
+            'accept_terms' => 'You must accept our Terms and Conditions',
+        ];
+    }
+}
