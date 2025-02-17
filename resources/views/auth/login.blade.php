a9a648f0a82c3e7a2e70c1222125d24200c79e06
Cedric Puchalver <cedric@season-of-mist.com>
2025-02-17 05:21:21 +0100

    Fichiers pour Sail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,77 +2,77 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
 
-        <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
-            <div class="p-4 sm:p-7">
-                <div class="text-center">
-                    <h1 class="block text-2xl font-bold text-gray-800">Sign in</h1>
-                    <p class="mt-2 text-sm text-gray-600">
-                        Don't have an account yet?
-                        <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="{{ route('register') }}">
-                        Sign up here
-                        </a>
-                    </p>
-                </div>
+    <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
+        <div class="p-4 sm:p-7">
+            <div class="text-center">
+                <h1 class="block text-2xl font-bold text-gray-800">{{ __('auth.login.heading') }}</h1>
+                <p class="mt-2 text-sm text-gray-600">
+                    {{ __('auth.login.no_account' ) }}
+                    <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="{{ route('register') }}">
+                        {{ __('auth.login.signup' ) }}
+                    </a>
+                </p>
+            </div>
 
-                <div class="mt-5">
-                    <!-- Form -->
-                    <form>
-                        <div class="grid gap-y-4">
-                            <!-- Form Group -->
-                            <div>
-                                <label for="email" class="block text-sm mb-2">{{ __('Email address') }}</label>
-                                <div class="relative">
-                                    <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="email-error">
-                                    <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                                        <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
-                                            <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                                        </svg>
-                                    </div>
-                                </div>
-                                <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
-                            </div>
-                            <!-- End Form Group -->
-
-                            <!-- Form Group -->
-                            <div>
-                                <div class="flex justify-between items-center">
-                                    <label for="password" class="block text-sm mb-2">{{ __('Password') }}</label>
-                                    @if (Route::has('password.request'))
-                                    <a class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="{{ route('password.request') }}">{{ __('Forgot your password?') }}</a>
-                                    @endif
-                                </div>
-                                <div class="relative">
-                                    <input type="password" id="password" name="password" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="password-error">
-                                    <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
-                                        <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+            <div class="mt-5">
+                <!-- Form -->
+                <form method="POST" action="{{ route('login') }}">
+                    @csrf
+                    <div class="grid gap-y-4">
+                        <!-- Form Group -->
+                        <div>
+                            <x-input-label for="email" class="mb-2" :value="__('auth.login.email')" />
+                            <div class="relative">
+                                <x-text-input type="email" id="email" name="email" class="w-full" aria-describedby="email-error" />
+                                <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                    <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
-                                        </svg>
-                                    </div>
-                                </div>
-                                <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
-                            </div>
-                            <!-- End Form Group -->
-
-                            <!-- Checkbox -->
-                            <div class="flex items-center">
-                                <div class="flex">
-                                    <input id="remember-me" name="remember" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500">
-                                </div>
-                                <div class="ms-3">
-                                    <label for="remember_me" class="text-sm">{{ __('Remember me') }}</label>
+                                    </svg>
                                 </div>
                             </div>
-                            <!-- End Checkbox -->
-
-                            <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">{{ __('Sign in') }}</button>
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
-                    </form>
-                    <!-- End Form -->
-                </div>
+                        <!-- End Form Group -->
+
+                        <!-- Form Group -->
+                        <div>
+                            <div class="flex justify-between items-center">
+                                <x-input-label for="password" class="mb-2" :value="__('auth.login.password')" />
+                                @if (Route::has('password.request'))
+                                <a class="inline-flex items-center gap-x-1 text-sm text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="{{ route('password.request') }}">{{ __('auth.login.forgot_password') }}</a>
+                                @endif
+                            </div>
+                            <div class="relative">
+                                <x-text-input type="password" id="password" name="password" class="w-full" aria-describedby="password-error" />
+                                <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
+                                    <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
+                                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+                                    </svg>
+                                </div>
+                            </div>
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+                        </div>
+                        <!-- End Form Group -->
+
+                        <!-- Checkbox -->
+                        <div class="flex items-center">
+                            <div class="flex">
+                                <x-checkbox-input id="remember-me" name="remember" />
+                            </div>
+                            <div class="ms-3">
+                                <x-input-label for="remember-me" :value="__('auth.login.remember')" />
+                            </div>
+                        </div>
+                        <!-- End Checkbox -->
+
+                        <x-primary-button class="w-full">
+                            {{ __('auth.login.button') }}
+                        </x-primary-button>
+                    </div>
+                </form>
+                <!-- End Form -->
             </div>
         </div>
-    </form>
+    </div>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,11 +5,11 @@
         <div class="mt-7 bg-white border border-gray-200 rounded-xl shadow-sm">
             <div class="p-4 sm:p-7">
                 <div class="text-center">
-                    <h1 class="block text-2xl font-bold text-gray-800">Sign up</h1>
+                    <h1 class="block text-2xl font-bold text-gray-800">{{ __('auth.register.heading') }}</h1>
                     <p class="mt-2 text-sm text-gray-600">
-                        {{ __('Already have an account?') }}
+                        {{ __('auth.register.already_account') }}
                         <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="{{ route('login') }}">
-                        {{ __('Sign in here') }}
+                        {{ __('auth.register.signin') }}
                         </a>
                     </p>
                 </div>
@@ -20,76 +20,78 @@
                         <div class="grid gap-y-4">
                             <!-- Form Group -->
                             <div>
-                                <label for="name" class="block text-sm mb-2">{{ __('Name') }}</label>
+                                <x-input-label for="name" class="mb-2" :value="__('auth.register.name')" />
                                 <div class="relative">
-                                    <input type="text" id="name" name="name" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" aria-describedby="name-error">
+                                    <x-text-input type="text" id="name" name="name" class="w-full" aria-describedby="name-error" />
                                     <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                         <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                         </svg>
                                     </div>
                                 </div>
-                                <p class="hidden text-xs text-red-600 mt-2" id="name-error">Please include your name</p>
+                                <p class="hidden text-xs text-red-600 mt-2" id="name-error">{{ __('auth.register.name_error') }}</p>
                             </div>
                             <!-- End Form Group -->
                              
                             <!-- Form Group -->
                             <div>
-                                <label for="email" class="block text-sm mb-2">{{ __('Email address') }}</label>
+                                <x-input-label for="email" class="mb-2" :value="__('auth.register.email')" />
                                 <div class="relative">
-                                    <input type="email" id="email" name="email" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="email-error">
+                                    <x-text-input type="email" id="email" name="email" class="w-full" required aria-describedby="email-error" />
                                     <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                         <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                         </svg>
                                     </div>
                                 </div>
-                                <p class="hidden text-xs text-red-600 mt-2" id="email-error">Please include a valid email address so we can get back to you</p>
+                                <p class="hidden text-xs text-red-600 mt-2" id="email-error">{{ __('auth.register.email_error') }}</p>
                             </div>
                             <!-- End Form Group -->
 
                             <!-- Form Group -->
                             <div>
-                                <label for="password" class="block text-sm mb-2">{{ __('Password') }}</label>
+                                <x-input-label for="password" class="mb-2" :value="__('auth.register.password')" />
                                 <div class="relative">
-                                    <input type="password" id="password" name="password" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="password-error">
+                                    <x-text-input type="password" id="password" name="password" class="w-full" required aria-describedby="password-error" />
                                     <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                         <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                         </svg>
                                     </div>
                                 </div>
-                                <p class="hidden text-xs text-red-600 mt-2" id="password-error">8+ characters required</p>
+                                <p class="hidden text-xs text-red-600 mt-2" id="password-error">{{ __('auth.register.password_error') }}</p>
                             </div>
                             <!-- End Form Group -->
 
                             <!-- Form Group -->
                             <div>
-                                <label for="password_confirmation" class="block text-sm mb-2">{{ ('Confirm Password') }}</label>
+                                <x-input-label for="confirm-password" class="mb-2" :value="__('auth.register.password_confirmation')" />
                                 <div class="relative">
-                                    <input type="password" id="confirm-password" name="password_confirmation" class="py-3 px-4 block w-full border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none" required aria-describedby="confirm-password-error">
+                                    <x-text-input type="password" id="confirm-password" name="password_confirmation" class="w-full" required aria-describedby="confirm-password-error" />
                                     <div class="hidden absolute inset-y-0 end-0 pointer-events-none pe-3">
                                         <svg class="size-5 text-red-500" width="16" height="16" fill="currentColor" viewBox="0 0 16 16" aria-hidden="true">
                                             <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8 4a.905.905 0 0 0-.9.995l.35 3.507a.552.552 0 0 0 1.1 0l.35-3.507A.905.905 0 0 0 8 4zm.002 6a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
                                         </svg>
                                     </div>
                                 </div>
-                                <p class="hidden text-xs text-red-600 mt-2" id="confirm-password-error">Password does not match the password</p>
+                                <p class="hidden text-xs text-red-600 mt-2" id="confirm-password-error">{{ __('auth.register.password_confirmation_error') }}</p>
                             </div>
                             <!-- End Form Group -->
 
                             <!-- Checkbox -->
                             <div class="flex items-center">
                                 <div class="flex">
-                                    <input id="remember-me" name="remember-me" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500">
+                                    <x-checkbox-input id="remember-me" name="remember-me" />
                                 </div>
                                 <div class="ms-3">
-                                    <label for="remember-me" class="text-sm">I accept the <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="#">Terms and Conditions</a></label>
+                                    <x-input-label for="remember-me">I accept the <a class="text-blue-600 decoration-2 hover:underline focus:outline-none focus:underline font-medium" href="#">Terms and Conditions</a></x-input-label>
                                 </div>
                             </div>
                             <!-- End Checkbox -->
 
-                            <button type="submit" class="w-full py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none">{{ __('Sign up') }}</button>
+                            <x-primary-button class="w-full">
+                                {{ __('auth.register.signup') }}
+                            </x-primary-button>
                         </div>
                     </form>
                     <!-- End Form -->
